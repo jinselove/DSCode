972dab53c4b3663b6086caa8510d2a2f30e9cd24
jinselove <jyli@uchicago.edu>
2018-10-01 13:31:53 -0500

    test algo: add test for implementation of information gain using Gini

--- test/test_tree_info_gain.py
+++ test/test_tree_info_gain.py
@@ -28,49 +28,66 @@ def train_x_y(csv_file):
     return train_x, train_y
 
 
-def test_init_correctly_with_split_mode_equals_to_random(train_x_y):
-    info_gain = InfoGain(train_x_y[0], train_x_y[1], split_mode="random")
-    assert_frame_equal(info_gain.train_x, train_x_y[0])
-    assert_series_equal(info_gain.train_y, train_x_y[1])
-    assert set(info_gain.split_threshold.keys()) == set(info_gain.train_x.columns)
-    assert info_gain.target_entropy == 1.0  # since we are using p*log2(p) to calculate entropy and in the test data,
-    # p=1/2
+def test_init_correctly_if_info_type_is_gini():
+    info_gain = InfoGain(info_type="gini", split_mode="random")
+    assert info_gain.info_type == "gini"
+    assert info_gain.params == {"split_mode": "random"}
+
+
+def test_init_correctly_if_none_info_type_is_given():
+    info_gain = InfoGain(split_mode="random")
+    assert info_gain.info_type == "entropy"
+
+
+def test_info_gain_can_give_the_correct_information_gain_using_entropy(train_x_y):
+    split_threshold = {"A": 5., "B": 3., "C": 4.2, "D": 1.4}
+    info_gain = InfoGain(info_type="entropy", split_threshold=split_threshold)
+    # the answer is from http://dataaspirant.com/2017/01/30/how-decision-tree-algorithm-works/
+    # note that the information gain for 'B' is wrong on the website
+    answer = {'A': 0.062278901396852215, 'B': 0.31127812445913283, 'C': 0.54879494069539858, 'D': 0.41882123107207492}
+    info_gain_dict = info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
+    assert info_gain_dict == pytest.approx(answer)
+
+
+def test_info_gain_can_give_the_correct_information_gain_using_gini(train_x_y):
+    split_threshold = {"A": 5., "B": 3., "C": 4.2, "D": 1.4}
+    info_gain = InfoGain(info_type="gini", split_threshold=split_threshold)
+    # the answer is from http://dataaspirant.com/2017/01/30/how-decision-tree-algorithm-works/
+    # note that the information gain for 'B' is wrong on the website
+    answer = {'A': 0.45833333333333326, 'B': 0.33333333333333331, 'C': 0.1999999999999999, 'D': 0.27272727272727271}
+    info_gain_dict = info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
+    assert info_gain_dict == pytest.approx(answer)
+
+
+def test_info_gain_run_through_when_split_mode_is_random(train_x_y):
+    info_gain = InfoGain(info_type="entropy", split_mode="random")
+    info_gain_dict = info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
+    info_gain_dict = info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
+    assert set(info_gain_dict.keys()) == set(train_x_y[0].columns)
+
+
+def test_info_gain_exit_when_wrong_info_type_is_requested(train_x_y, capfd):
+    info_gain = InfoGain(info_type="xxx", split_mode="random")
+    with pytest.raises(SystemExit):
+            info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
+    out, _ = capfd.readouterr()
+    assert "Error: info_type" in out
 
 
 def test_init_exit_with_wrong_split_mode(train_x_y, capfd):
+    info_gain = InfoGain(info_type="entropy", split_mode="xxx")
     with pytest.raises(SystemExit):
-        info_gain = InfoGain(train_x_y[0], train_x_y[1], split_mode="xxxx")
+        info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
     out, _ = capfd.readouterr()
     assert "Error: split mode" in out
 
 
-def test_init_correctly_with_split_threshold_is_given(train_x_y):
-    split_threshold = {"A": 5., "B": 3., "C": 4.2, "D": 1.4}
-    info_gain = InfoGain(train_x_y[0], train_x_y[1], split_threshold=split_threshold)
-    assert_frame_equal(info_gain.train_x, train_x_y[0])
-    assert_series_equal(info_gain.train_y, train_x_y[1])
-    assert info_gain.split_threshold == split_threshold
-    assert info_gain.target_entropy == 1.0  # since we are using p*log2(p) to calculate entropy and in the test data,
-    # p=1/2
-
-
 def test_init_exit_if_neither_split_mode_nor_split_threshold_is_given(train_x_y, capfd):
+    info_gain = InfoGain(info_type="entropy")
     with pytest.raises(SystemExit):
-        info_gain = InfoGain(train_x_y[0], train_x_y[1])
-
+        info_gain.cal_info_gain(train_x_y[0], train_x_y[1])
     out, _ = capfd.readouterr()
     assert "Error: if split_mode is not given" in out
-
-
-def test_info_gain_can_give_the_correct_information_gain(train_x_y):
-    split_threshold = {"A": 5., "B": 3., "C": 4.2, "D": 1.4}
-    info_gain = InfoGain(train_x_y[0], train_x_y[1], split_threshold=split_threshold)
-    # the answer is from http://dataaspirant.com/2017/01/30/how-decision-tree-algorithm-works/
-    # note that the information gain for 'B' is wrong on the website
-    answer = {'A': 0.062278901396852215, 'B': 0.31127812445913283, 'C': 0.54879494069539858, 'D': 0.41882123107207492}
-    assert info_gain.information_gain == pytest.approx(answer)
-
-
 
 
 
